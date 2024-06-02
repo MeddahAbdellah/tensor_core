0b3e5c06de3b3e30e12acb04fa8867fda6f4f201
Abdallah MEDDAH <meddah.abdellah.spcx@gmail.com>
2024-06-02 16:07:18 +0200

    changes

--- tensor_core.sv
+++ tensor_core.sv
@@ -1,2 +1,12 @@
-module tensor_core ();
+module tensor_core (
+    input logic [15:0] A[15:0], // 16 inputs of 16 bits each
+    input logic [15:0] B[15:0], // 16 inputs of 16 bits each
+    output logic [31:0] C[15:0] // 16 outputs of 32 bits each
+);
+    matrix4x4mult matrix4x4mult_instance (
+        .A(A),
+        .B(B),
+        .C(C)
+    );
+
 endmodule
