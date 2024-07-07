9ff5540546e1e87625c3d442bf64400189c68939
MeddahAbdellah <meddah.abdellah.spcx@gmail.com>
2024-07-07 20:31:29 +0200

    update

--- matcher.sv
+++ matcher.sv
@@ -13,10 +13,10 @@ module matcher#(
     logic found;
 
     logic [ADDR_WIDTH-1: 0] start_addr = 0;
-    logic [ADDR_WIDTH-1: 0] end_addr = 4'b1111;
+    logic [ADDR_WIDTH: 0] end_addr = 5'b10000;
 
     logic [1:0] state = 0;
-    logic [ADDR_WIDTH-1: 0] av = 0;
+    logic [ADDR_WIDTH: 0] av = 0;
     logic [ADDR_WIDTH-1: 0] ai;
 
     logic vocab_overflow;
