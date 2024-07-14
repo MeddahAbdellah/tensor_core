9d16ec6baaa3bcfbc9e6baad2eeafbba9df9ca76
MeddahAbdellah <meddah.abdellah.spcx@gmail.com>
2024-07-14 20:05:19 +0200

    update

--- encoder.sv
+++ encoder.sv
@@ -52,7 +52,7 @@ module encoder#(
     sram #(
         .DATA_WIDTH(DATA_WIDTH),
         .ADDR_WIDTH(ADDR_WIDTH),
-        .INIT_FILE("C:\\Users\\abdal\\verilog_work\\tensor_core\\vocab.bin")
+        .INIT_FILE("C:\\Users\\abdal\\verilog_work\\tensor_core\\code.bin")
     ) code_ram (
         .clk(clk),
         .cs(1'b1),
